63a0198298b71980a1b1d86f7c80a21cb0d08f4e
SammyCui <35609860+SammyCui@users.noreply.github.com>
2018-09-26 10:13:21 +0200

    PlayerControl

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerControl : MonoBehaviour {
+    
+    //Some Parameters
+
+    //speed
+    public static float speed;
+
+    //jumpheight
+    public static float jumpheight;
+
+    private Rigidbody2D rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = transform.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    
+}
