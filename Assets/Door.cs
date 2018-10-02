23801d9b319bb97a30dd886d93148c757b342b59
SammyCui <35609860+SammyCui@users.noreply.github.com>
2018-10-02 10:14:12 +0200

    Exit

---
+++ Assets/Door.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour {
+
+
+    int CurrentScene = 0;
+
+	// Use this for initialization
+	void Start () {
+        CurrentScene = SceneManager.GetActiveScene().buildIndex;
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        SceneManager.LoadScene(CurrentScene+1);
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
